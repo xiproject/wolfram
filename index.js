fe24a54251010a4d664d18bbd15005d58eac7b8f
Mihir Rege <mihirrege@gmail.com>
2015-01-16 01:35:56 +0530

    Split files

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 var xal = require('../../xal-javascript');
-var nlpparser = require('speakeasy-nlp');
-var xpath = require('xpath');
-var dom = require('xmldom').DOMParser;
 var _ = require('underscore');
-
-var config = require('./config.json');
-
-var request;
-
-if (config.proxy) {
-    request = require('request').defaults({
-        proxy: config.proxy
-    });
-} else {
-    request = require('request');
-}
-
-var query_url = "http://api.wolframalpha.com/v2/query?appid=" + config.auth + "&input=";
-
-
+var nlpparser = require('speakeasy-nlp');
+var wolfram = require('./wolfram');
 
 function containsObject(obj, list) {
     var i;
@@ -35,25 +18,7 @@ function isQuestion(query) {
     return containsObject(nlpparser.classify(query).action, question_actions);
 }
 
-function analyzeXML(xml) {
-    var doc = new dom().parseFromString(xml);
-    var nodes = xpath.select("//pod[@title='Result']/subpod/plaintext", doc);
-    var result = null;
-    if (nodes.length !== 0) {
-        result = nodes[0].firstChild.data;
-    }
-    return result;
-}
-
-function query(q, cb) {
-    request(query_url + q, function(error, response, body) {
-        cb(analyzeXML(body));
-    });
-}
-
-
 xal.on('xi.event.input.text', function(state, done) {
-
     var texts = state.get('xi.event.input.text');
     var text = _.reduce(texts, function(memo, value) {
         if (memo.certainty > value.certainty) {
@@ -63,7 +28,11 @@ xal.on('xi.event.input.text', function(state, done) {
     }).value;
 
     if (isQuestion(text)) {
-        query(text, function(answer) {
+        wolfram.query(text, function(err, answer) {
+            if(err){
+                xal.log.error(err);
+                return;
+            }
             if (answer) {
                 xal.createEvent('xi.event.output.text', function(state, done) {
                     xal.log.info({
@@ -72,6 +41,9 @@ xal.on('xi.event.input.text', function(state, done) {
                     state.put('xi.event.output.text', answer);
                     done(state);
                 });
+            }
+            else{
+                xal.log.debug('Queried but did not get relevant answer');
             }
         });
     }
---
+++ wolfram.js
@@ -0,0 +1,44 @@
+var xpath = require('xpath');
+var dom = require('xmldom').DOMParser;
+var config = require('./config.json');
+
+var request;
+
+if (config.proxy) {
+    request = require('request').defaults({
+        proxy: config.proxy
+    });
+} else {
+    request = require('request');
+}
+
+var query_url = "http://api.wolframalpha.com/v2/query?format=plaintext&primary=true&appid=" + config.auth + "&input=";
+
+
+
+
+
+function analyzeXML(xml) {
+    var doc = new dom().parseFromString(xml);
+    var nodes = xpath.select("//pod[@title='Result']/subpod/plaintext", doc);
+    var result = null;
+    if (nodes.length !== 0) {
+        result = nodes[0].firstChild.data;
+    }
+    return result;
+}
+
+function query(q, cb) {
+    request(query_url + encodeURIComponent(q), function(err, response, body) {
+        if(err) {
+            cb(err);
+        }
+        else{
+            cb(null, analyzeXML(body));
+        }
+    });
+}
+
+
+
+exports.query = query;
