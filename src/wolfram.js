236320897ad8ea6968c52ed433769a3520b5f376
Mihir Rege <mihirrege@gmail.com>
2015-03-24 19:46:27 +0530

    Move wolfram.js to src/wolfram

--- src/wolfram.js
+++ src/wolfram.js
@@ -14,31 +14,37 @@ if (config.proxy) {
 
 var query_url = "http://api.wolframalpha.com/v2/query?format=plaintext&primary=true&appid=" + config.auth + "&input=";
 
-
-
-
-
 function analyzeXML(xml) {
     var doc = new dom().parseFromString(xml);
     var nodes = xpath.select("//pod[@title='Result']/subpod/plaintext", doc);
     var result = null;
-    if (nodes.length !== 0) {
+    if (nodes.length !== 0 && nodes[0].firstChild) {
         result = nodes[0].firstChild.data;
     }
     return result;
 }
 
 function query(q, cb) {
-    request(query_url + encodeURIComponent(q), function(err, response, body) {
-        if(err) {
-            cb(err);
-        }
-        else{
-            cb(null, analyzeXML(body));
-        }
-    });
+
+    var tries = 0;
+    var maxTries = 3;
+
+    var queryHelper = function () {
+        request(query_url + encodeURIComponent(q), function(err, response, body) {
+            if(err) {
+                if (tries < maxTries){
+                    tries += 1;
+                    queryHelper();
+                } else {
+                    cb(err);
+                }
+
+            } else {
+                cb(null, analyzeXML(body));
+            }
+        });
+    };
+    queryHelper();
 }
-
-
 
 exports.query = query;
